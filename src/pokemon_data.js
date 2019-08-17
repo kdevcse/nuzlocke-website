7d73a12ded07108b5d07c69575361947aaccf3cf
klewiscse <klewiscse@gmail.com>
2019-08-17 17:44:53 -0500

    Reconfigure React Web Framework
    - React Framework more customizable to multipage websites

---
+++ src/pokemon_data.js
@@ -0,0 +1,19 @@
+'use strict';
+
+class PokemonData extends React.Component {
+    //react constructor: constructor() with JS consructor: super()
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+        <div>
+            Hello how is it going? I am a react application.
+        </div>
+        );
+    }
+}
+
+let domContainer = document.querySelector('.pokemon');
+ReactDOM.render(<PokemonData />, domContainer);

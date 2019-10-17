3ccf461eecce0ac52375a18a209a156cb12d0a3a
Kevin <klewiscse@gmail.com>
2019-10-16 22:52:57 -0500

    Convert to polymer

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import NavBar from './components/navbar.js'
-import PokemonData from './components/pokemon_data.js'
-
-ReactDOM.render(<NavBar/>,document.getElementById('nav-container'));
-
-let domContainer = document.querySelector('.pokemon');
-ReactDOM.render(<PokemonData />, domContainer);
--- src/view/view.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-import ReactDom from "react-dom"
-import NavBar from './components/navbar.js'
-
-ReactDOM.render(<NavBar/>,document.getElementById('nav-container'));

f0eb3de82379958c055428cac69aea13cf1542a8
Kevin <klewiscse@gmail.com>
2019-11-05 00:44:51 -0600

    Update Dashboard.tsx

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -2,18 +2,41 @@ import React from 'react';
 import './Dashboard.css';
 import PartyPokemon from '../Components/Party/PartyPokemon';
 
-class Dashboard extends React.Component {
+interface PokemonParty{
+  First: string;
+  Second: string;
+  Third: string;
+  Fourth: string;
+  Fifth: string;
+  Sixth: string;
+}
+
+class Dashboard extends React.Component<PokemonParty> {
+  state: PokemonParty;
+  constructor(props: PokemonParty){
+    super(props);
+    this.state = {First: 'Pikachu', Second: 'Zapdos', Third: 'Mewtwo', Fourth: 'Tentacruel', Fifth: 'Jigglypuff', Sixth: 'Onix'};
+  }
+  componentDidMount(){
+    setInterval(() => {
+      if(this.state.First === 'Pikachu'){
+        this.setState({First: 'Charmander'});
+      } else {
+        this.setState({First: 'Pikachu'});
+      }
+    },10000);
+  }
   render(){
     return (
       <div className="Dashboard">
         <h1>Dashboard</h1>
         <div className='PartyContainer'>
-          <PartyPokemon Pokemon='Pikachu'></PartyPokemon>
-          <PartyPokemon Pokemon='Zapdos'></PartyPokemon>
-          <PartyPokemon Pokemon='Mewtwo'></PartyPokemon>
-          <PartyPokemon Pokemon='Tentacruel'></PartyPokemon>
-          <PartyPokemon Pokemon='Jigglypuff'></PartyPokemon>
-          <PartyPokemon Pokemon='Onix'></PartyPokemon>
+          <PartyPokemon Pokemon={this.state.First}></PartyPokemon>
+          <PartyPokemon Pokemon={this.state.Second}></PartyPokemon>
+          <PartyPokemon Pokemon={this.state.Third}></PartyPokemon>
+          <PartyPokemon Pokemon={this.state.Fourth}></PartyPokemon>
+          <PartyPokemon Pokemon={this.state.Fifth}></PartyPokemon>
+          <PartyPokemon Pokemon={this.state.Sixth}></PartyPokemon>
         </div>
       </div>
     );

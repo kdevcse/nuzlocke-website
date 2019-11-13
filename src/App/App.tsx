66bc3f9ca04dedef88272c6b4ada4f7425f20c7c
Kevin <klewiscse@gmail.com>
2019-11-13 02:58:26 -0600

    Fix routing issues

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -39,8 +39,8 @@ class App extends React.Component<{}, IState>{
           <Nav runs={this.state.runs}/>
           <div className="content">
             <Switch>
-              <Route path="/" exact component={Dashboard}/>
-              <Route path="/dashboard" exact component={Dashboard}/>
+              <Route path="/" exact render={() => <Dashboard runs={this.state.runs}/>}/>
+              <Route path="/dashboard" exact render={() => <Dashboard runs={this.state.runs}/>}/>
               <Route path="/dashboard/:runid" exact render={({match}) => <NuzzyRun run={this.state.runs.find(x => x.RunTitle === match.params.runid)}/>}/>
               <Route path="/login" component={Login}/>
               <Route path="/about" component={About}/>
--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import './Dashboard.css';
 import { Link } from 'react-router-dom';
+import { NuzInfo } from './NuzzyRun';
 
-function Dashboard(){
+export function Dashboard(props: any){
     //TODO: Here we will likely need to generate a dynamic list of links to all of our nuzlocke runs
-
     return (
         <div className="dashboard">
             <h2>Runs:</h2>
-            <div className="dashboard-run-container">
-                <Link to='/dashboard/NuzlockeRun1'>
-                    <p className='NavLogin'>NuzlockeRun1</p>
-                </Link>
-                <button>Add Run</button>
+            <div  className="dashboard-run-container">
+                {gatherRuns(props.runs)}
             </div>
+            <button>Add Run</button>
         </div>
     );
+}
+
+function gatherRuns(runs: NuzInfo[]){
+    return runs.map(run => (
+        <Link key={run.RunTitle} to={`/dashboard/${run.RunTitle}`}>
+            <p className='NavLogin'>{run.RunTitle}</p>
+        </Link>
+    ));
 }
 
 export default Dashboard;

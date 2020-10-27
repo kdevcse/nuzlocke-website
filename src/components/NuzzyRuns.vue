c666a07ac2947d2282b4420b0dcfb0499fa4c5f2
Kevin <klewiscse@gmail.com>
2020-10-27 03:10:01 -0500

    Use CSS to fix trash hover bug

--- src/components/NuzzyRuns.vue
+++ src/components/NuzzyRuns.vue
@@ -10,10 +10,9 @@
         <template #header>
           <div class="align-middle header-container">
             <span class="header-title" @click="openRun(run.run_id)">{{run.name}}</span>
-            <div class="delete-run-button" @mouseenter="fillTrash(run, true)" @mouseleave="fillTrash(run, false)"
-            @click="deleteRun(run.run_id, run.name)">
-              <b-icon v-if="getFillStatus(run)" title="Delete run" icon="trash-fill" class="run-trash-icon"></b-icon>
-              <b-icon v-else icon="trash" class="run-trash-icon"></b-icon>
+            <div class="delete-run-button" @click="deleteRun(run.run_id, run.name)">
+              <b-icon title="Delete run" icon="trash-fill" class="run-trash-icon-fill"></b-icon>
+              <b-icon title="Delete run" icon="trash" class="run-trash-icon"></b-icon>
             </div>
           </div>
         </template>
@@ -35,11 +34,6 @@ export default {
   },
   data: function() {
     return {
-      trashIsHovered: false,
-      nuzzyRuns: this.runs.map(r => {
-        r.trashIsHovered = false;
-        return r;
-      }),
       runToDelete: { name: '', id: ''}
     }
   },
@@ -54,22 +48,6 @@ export default {
         id: runId
       };
       this.$bvModal.show('delete-run-window');
-    },
-    fillTrash(run, needsToFill){
-      for(let i = 0; i < this.nuzzyRuns.length; i++) {
-        if(this.nuzzyRuns[i].run_id === run.run_id) {
-          let copyOfRun = this.nuzzyRuns[i];
-          copyOfRun.trashIsHovered = needsToFill;
-          this.$set(this.nuzzyRuns, i, copyOfRun);
-        }
-      }
-    },
-    getFillStatus(run){
-      for(let i = 0; i < this.nuzzyRuns.length; i++) {
-        if(this.nuzzyRuns[i].run_id === run.run_id) {
-          return this.nuzzyRuns[i].trashIsHovered;
-        }
-      }
     }
 	}
 }
@@ -92,5 +70,17 @@ export default {
 .delete-run-button {
   float: right;
   cursor: pointer;
+}
+.delete-run-button > .run-trash-icon-fill {
+  display: none;
+}
+.delete-run-button > .run-trash-icon {
+  display: inherit;
+}
+.delete-run-button:hover > .run-trash-icon-fill {
+  display: inherit;
+}
+.delete-run-button:hover > .run-trash-icon {
+  display: none;
 }
 </style>

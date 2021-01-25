c93ae0171ab59169c2554fef39cdcb1e9f7c8c9a
klewiscse <klewiscse@gmail.com>
2021-01-25 01:56:21 -0600

    Add more functionality to toolbar

--- src/components/PokeCardToolbar.vue
+++ src/components/PokeCardToolbar.vue
@@ -16,6 +16,15 @@
           <b-icon icon="chevron-double-up"></b-icon>
         </b-button>
         <b-button
+          v-if="canMoveToBox"
+          @click="onPokeMoveToBox"
+          variant="info"
+          title="Move to Box"
+          :disabled="!canPress">
+          <b-icon icon="box"></b-icon>
+        </b-button>
+        <b-button
+          v-if="isNotDead"
           variant="danger"
           title="Death"
           @click="onPokeDeath"
@@ -47,6 +56,12 @@ export default {
   computed: {
     canPress() {
       return !this.demo && this.pokedata && this.pokedata.id;
+    },
+    canMoveToBox() {
+      return this.pokedata ? this.pokedata.party > -1 : false;
+    },
+    isNotDead() {
+      return !this.pokedata.death;
     }
   },
   methods: {
@@ -56,6 +71,15 @@ export default {
       const pokemon = new Pokemon();
       pokemon.setValuesFromPokeDataObj(this.pokedata);
       pokemon.lvl += 1;
+
+      firestore().collection(pokemonQuery).doc(pokemon.id).update(pokemon.object);
+    },
+    onPokeMoveToBox() {
+      const runQuery = `users/${auth().currentUser.uid}/runs/${this.runId}`;
+      const pokemonQuery = `${runQuery}/pokemon`;
+      const pokemon = new Pokemon();
+      pokemon.setValuesFromPokeDataObj(this.pokedata);
+      pokemon.party = -1;
 
       firestore().collection(pokemonQuery).doc(pokemon.id).update(pokemon.object);
     },
